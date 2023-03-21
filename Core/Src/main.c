249cd3660c74915e810205120410071f76ed0204
sohal <tosohal@gmail.com>
2023-03-21 06:29:41 +0100

    With two interrupts

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,7 +56,18 @@ static void MX_TIM1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+static volatile uint32_t timCnt;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if(htim == &htim1)
+  {
+    if(timCnt++ > 1)
+    {
+      HAL_GPIO_TogglePin(LD4_GPIO_Port, LD4_Pin);
+      timCnt = 0;
+    }
+  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -89,7 +100,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -97,7 +108,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+    HAL_Delay(200);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
