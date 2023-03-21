64e4e42fc0ec7a53789290d1755a09c0f380d4c3
sohal <tosohal@gmail.com>
2023-03-21 21:29:32 +0100

    remove ide project
    Add qpc with static build options

--- cmake/lib_qpc.cmake
+++ cmake/lib_qpc.cmake
@@ -26,7 +26,7 @@ message(FATAL_ERROR "QPC can not be integrated with this compiler...")
 endif()
 cmake_print_variables(QPC_COMPILER_NAME)
 if(NOT DEFINED qpc_SOURCE_DIR)
-set(qpc_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../qpc)
+message(FATAL_ERROR "QPC Framework not downloaded.")
 endif()
 cmake_print_variables(qpc_SOURCE_DIR)
 
@@ -51,11 +51,11 @@ set(qpc_qk_SOURCES
 
 set(qpc_qs_SOURCES
     ${qpc_SOURCE_DIR}/src/qs/qs.c
-    ${qpc_SOURCE_DIR}/src/qs/qs_64bit.c
+#    ${qpc_SOURCE_DIR}/src/qs/qs_64bit.c
     ${qpc_SOURCE_DIR}/src/qs/qs_fp.c
     ${qpc_SOURCE_DIR}/src/qs/qs_rx.c
-    ${qpc_SOURCE_DIR}/src/qs/qstamp.c
-    ${qpc_SOURCE_DIR}/src/qs/qutest.c
+#    ${qpc_SOURCE_DIR}/src/qs/qstamp.c
+#    ${qpc_SOURCE_DIR}/src/qs/qutest.c
 )
 
 set(qpc_qv_SOURCES
@@ -69,6 +69,34 @@ set(qpc_qxk_SOURCES
     ${qpc_SOURCE_DIR}/src/qxk/qxk_xthr.c
 )
 
+set(qpc_ports_qk_PATH "${qpc_SOURCE_DIR}/ports/arm-cm/qk/${QPC_COMPILER_NAME}")
 set(qpc_ports_arm-cm_qk_SOURCES
-    ${qpc_SOURCE_DIR}/ports/arm-cm/qk/${QPC_COMPILER_NAME}/
+    ${qpc_ports_qk_PATH}/qk_port.c
+)
+
+add_library(qpc STATIC)
+
+target_sources(qpc
+    PRIVATE
+    ${qpc_qf_SOURCES}
+    ${qpc_qk_SOURCES}
+    ${qpc_ports_arm-cm_qk_SOURCES}
+)
+
+target_include_directories(qpc
+    PRIVATE
+    ${qpc_SOURCE_DIR}/include
+    ${qpc_ports_qk_PATH}
+)
+
+target_compile_options(qpc
+    PRIVATE
+    -mcpu=cortex-m0 #ARM_CPU (cortex-m0, cortex-m1...)
+                    #ARM_FPU (vfp)
+                    #FLOAT_ABI (soft|softfp|hard)
+    -mthumb
+    -Wall
+    -ffunction-sections
+    -fdata-sections
+    -O2
 )
